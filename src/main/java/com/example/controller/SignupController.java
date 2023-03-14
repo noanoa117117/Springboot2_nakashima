63a6e8ed8b495e88c44436db6f3999e7c90cde13
noanoa117117 <sinigamiyuuna@gmail.com>
2023-03-14 19:32:18 +0900

    6.2.2

--- src/main/java/com/example/controller/SignupController.java
+++ src/main/java/com/example/controller/SignupController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,25 +19,31 @@ import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping("/user")
-@Slf4j //簡単にログ出力
+@Slf4j // 簡単にログ出力
 public class SignupController {
-	
+
 	@Autowired
 	private UserApplicationService userApplicationService;
-	
-	/*ユーザー登録画面を表示*/
-	@GetMapping("/signup") //@ModelAttributeはmodel.addAttribute("signupForm",form)みたいな
-	public String getSignup(Model model,Locale locale,@ModelAttribute SignupForm form) {
-		//性別を取得
-		Map<String,Integer> genderMap = userApplicationService.getGenderMap(locale);
-		model.addAttribute("genderMap",genderMap);
-		
+
+	/* ユーザー登録画面を表示 */
+	@GetMapping("/signup") // @ModelAttributeはmodel.addAttribute("signupForm",form)みたいな
+	public String getSignup(Model model, Locale locale, @ModelAttribute SignupForm form) {
+		// 性別を取得
+		Map<String, Integer> genderMap = userApplicationService.getGenderMap(locale);
+		model.addAttribute("genderMap", genderMap);
+
 		return "user/signup";
 	}
-	
-	/*ユーザー登録処理*/
-	@PostMapping("/signup")
-	public String postSignup(@ModelAttribute SignupForm form) {
+
+	/* ユーザー登録処理 */
+	@PostMapping("/signup") //BindingResultでバインド、バリデーションエラー確認
+	public String postSignup(Model model, Locale locale, @ModelAttribute SignupForm form, BindingResult bindingResult) {
+		//入力チェック結果
+		if(bindingResult.hasErrors()) {
+			return getSignup(model,locale,form);
+		}
+		
+		
 		log.info(form.toString());
 		return "redirect:/login";
 	}
