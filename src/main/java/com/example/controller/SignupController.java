9d4b58eabb26fb93d1c282f9c9f328e1e764e949
noanoa117117 <sinigamiyuuna@gmail.com>
2023-03-18 01:42:39 +0900

    8.2.1

--- src/main/java/com/example/controller/SignupController.java
+++ src/main/java/com/example/controller/SignupController.java
@@ -3,6 +3,7 @@ package com.example.controller;
 import java.util.Locale;
 import java.util.Map;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.application.service.UserApplicationService;
+import com.example.domain.user.model.MUser;
+import com.example.domain.user.service.UserService;
 import com.example.form.GroupOrder;
 import com.example.form.SignupForm;
 
@@ -26,7 +29,13 @@ public class SignupController {
 
 	@Autowired
 	private UserApplicationService userApplicationService;
-
+	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private ModelMapper modelMapper;
+	
 	/* ユーザー登録画面を表示 */
 	@GetMapping("/signup") // @ModelAttributeはmodel.addAttribute("signupForm",form)みたいな
 	public String getSignup(Model model, Locale locale, @ModelAttribute SignupForm form) {
@@ -49,6 +58,12 @@ public class SignupController {
 		
 		
 		log.info(form.toString());
+		
+		/*formをMUserクラスに変換、mpdelmapperんもmapメソッドは簡単にフィールドコピー*/
+		MUser user = modelMapper.map(form, MUser.class);
+		//ユーザー登録
+		userService.signup(user);
+		
 		return "redirect:/login";
 	}
 }
